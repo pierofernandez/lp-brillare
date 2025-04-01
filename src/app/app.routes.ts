db99aa4d6ad60d3ff5b9ccd7436ecdb32866d206
Piero <pierofernandezz48@gmail.com>
2025-03-31 20:15:29 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, // Página principal
+    { path: 'about', component: AboutComponent }, // Página "Sobre nosotros"
+    { path: 'contact', component: ContactComponent }, // Página de contacto
+  ];
+  
+  @NgModule({
+      imports: [RouterModule.forRoot(routes)],
+      exports: [RouterModule]
+    })
+export class AppRoutingModule { }
